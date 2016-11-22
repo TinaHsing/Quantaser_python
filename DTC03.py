27b99e4738e47074c7960dae6aa35fce32dc0454
TinaHsing <tina@quantaser.com>
2016-11-22 22:45:33 +0800

    ADD keithley 2000 into instrument list
    1. Add Keithley 2000 into instrument list
    2. Add COM port seeing in the front panel
    3. slightly modified the panel layout

--- DTC03.py
+++ DTC03.py
@@ -21,17 +21,13 @@ sf = IntVar()
 enstatus =1
 master.title("DTC03 Testing")
 
-rm = visa.ResourceManager()
-dmm = rm.open_resource('ASRL7::INSTR')
-dmm.stop_bits=constants.StopBits.two
-dmm.write("*CLS")
-dmm.write("SYST:REM")
+
 
 x1 =0
 z=[]
 yplot1 =0
 boolupdate = 0
-w = Canvas(master, width=canvas_width,height=canvas_height, bg= "white")
+w = Canvas(master, width=canvas_width,height=canvas_height, bg= "black")
 def runvisa():
 	global taking
 	global x1
@@ -45,6 +41,10 @@ def runvisa():
 	global enstatus
 	global sf
 	taking =1
+	str_rem ='SYST:REM'
+	str_measv ='MEAS:VOLT:DC?'
+	str_measr ='MEAS:RES?'
+
 	w.delete("all")
 	totaltime = int(maxtime.get())
 	dt=float(steptime.get())
@@ -53,6 +53,27 @@ def runvisa():
 	else:
 		dt =0
 	plotxrange = int(totaltime/(dt+0.5))
+
+	
+	resourcename ='ASRL'
+	resourcename+=comport.get()
+	resourcename+='::INSTR'
+	print(resourcename)
+	rm = visa.ResourceManager()
+	dmm = rm.open_resource(resourcename)
+
+	if(instname.get() =="HP34401A"):
+		dmm.stop_bits=constants.StopBits.two
+	
+	else:
+		#dmm.stop_bits=constants.StopBits.one
+		str_rem =':'+str_rem
+		str_measr=':'+str_measr
+		str_measv =':'+str_measv
+	dmm.write("*CLS")
+	dmm.write(str_rem)
+
+		
 	
 	if(sf.get()==1):
 		fps = filepath.get()
@@ -87,12 +108,14 @@ def runvisa():
 			return		
 		if(var.get() == 1):
 
-			y=dmm.query("MEAS:VOLT:DC?")
+			y=dmm.query(str_measv)
+			y=1
 
 			tempact =(1/(math.log((float(y))/2.0)/3988+0.003354))-273.15
 			
 		else :
-			y=dmm.query("MEAS:RES?")
+			y=dmm.query(str_measr)
+			y=1
 			tempact =1/ (math.log(float(y)/10000)/3988+0.003354)-273.15
 
 		z.append(tempact)
@@ -101,7 +124,7 @@ def runvisa():
 
 
 		if ((index > 0) & (yplot>=0) & (yplot <= canvas_height) &(yplot1 >=0) & (yplot <=canvas_height)):
-			w.create_line(x1, yplot1, x, yplot, fill = "blue", width =3)
+			w.create_line(x1, yplot1, x, yplot, fill = "yellow", width =3)
 		x1 = x
 		yplot1 = yplot
 
@@ -164,11 +187,6 @@ def stopbutton():
 	enstatus =0
 
 
-def paste(self, event):
-	text = self.selection_get(selection='CLIPBOARD')
-	self.insert('insert', text)
-
-
 
 		
 runbutton = Button(master, text ="Run", command = runvisa)
@@ -187,10 +205,17 @@ texty4 = Label(master, text = str(yvalue4))
 tact_l = Label(master, text = "Tact:")
 tact = Label(master, text = "%3.4f" %(25), bg="black", fg="yellow", font=("Arial",18))
 sn_l = Label(master, text="S/N:")
+comport_l = Label(master, text ="ASRL")
 
 filepath = Entry(master)
-ymax = Entry(master, width = 6)
-ymin = Entry(master, width = 6)
+comport = Entry(master, width = 6)
+
+instname = StringVar(master)
+instname.set("HP34401A")
+instsele = OptionMenu(master, instname, "HP34401A", "Keithley2000")
+instsele.config(width =15)
+ymax = Entry(master, width = 6, justify='right')
+ymin = Entry(master, width = 6, justify='right')
 maxtime = Entry(master, width = 15)
 steptime = Entry(master, width =15)
 sn = Entry(master, width= 15)
@@ -198,29 +223,38 @@ measureV = Radiobutton(master, text = "measure voltage", variable = var, value =
 measureR = Radiobutton(master, text = "measure resistor", variable = var, value =2)
 
 filepath_l.grid(row=0, column =0, sticky =W)
-filepath.grid(row =0, column = 1, columnspan =2, sticky = EW)
-sn_l.grid(row =0, column =3, sticky =E)
-sn.grid(row=0, column =4, sticky =W)
-savef.grid(row = 0, column =5)
+filepath.grid(row =0, column = 1, columnspan =3, sticky = EW)
+sn_l.grid(row =0, column =4, sticky =E)
+sn.grid(row=0, column =5, sticky =W)
+instsele.grid(row=0, column=6)
 
+savef.grid(row = 1, column =0)
 maxt_l.grid(row =1, column =1,  sticky =E)
 maxtime.grid(row=1, column =2, sticky =W)
 measureV.grid(row = 1, column =3)
 tact_l.grid(row=1, column =4)
-runbutton.grid(row=1, column=5)
+tact.grid(row=1, column =5, sticky = EW)
+runbutton.grid(row=1, column=6)
+
+
+
+
 updatebutton.grid(row=2, column=0)
 step_l.grid(row=2, column=1, sticky=E)
 steptime.grid(row=2, column =2, sticky =W)
 measureR.grid(row = 2, column =3)
-tact.grid(row=2, column =4, sticky = EW)
-stopbutton.grid(row =2, column=5)
-ymax.grid(row =3, column=0, sticky =N)
-texty2.grid(row=4,column =0, sticky= NW)
-texty3.grid(row=5,column =0, sticky =W)
-texty4.grid(row=6,column =0, sticky= SW)
-ymin.grid(row =7, column=0, sticky= S)
+comport_l.grid(row=2, column =4, sticky = E)
+comport.grid(row=2, column=5, sticky =W)
+stopbutton.grid(row =2, column=6)
 
-w.grid(row=3 ,column=1, rowspan =5,columnspan=5)
+
+ymax.grid(row =3, column=0, sticky =NE)
+texty2.grid(row=4,column =0, sticky= NE)
+texty3.grid(row=5,column =0, sticky =E)
+texty4.grid(row=6,column =0, sticky= SE)
+ymin.grid(row =7, column=0, sticky= SE)
+
+w.grid(row=3 ,column=1, rowspan =5,columnspan=6)
 ymax.insert(0, str(default_ymax))
 ymin.insert(0, str(default_ymin))
 
